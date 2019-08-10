9b148dc6cba0b9ad7cb60c702a02217facb36441
BrendaOrtega <bremin11.20.93@gmail.com>
2019-08-10 18:28:39 -0500

    metas

--- src/components/Language.js
+++ src/components/Language.js
@@ -6,6 +6,15 @@ class Language extends Component {
     render() {
         return (
             <div className="language">
+                <meta
+                    name="description"
+                    content="Arango | Cocina de Raíz"
+                />
+                <meta
+                    name="keywords"
+                    content="arango, arango restaurante, cocina, cocina gourmet, comida mexicana, comida"
+                />
+                <title>Arango</title>
                 <div className="cubierta">
                 <div className="welcome">
                     <h2 className="float">¿En qué idioma prefieres visitar nuestro sitio?</h2>
